2711623a97b9fe0ead5c1fde55832a6f278c7ae7
mffonua <108596346+mffonua@users.noreply.github.com>
2023-01-15 02:29:03 -0700

    setting up app.js and starting navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+const [selectedPage, setSelectedPage] = useState('home'); /*Detemine what part of nav. we are at  */
+const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)"); /*Determine if min width is > or < 1060 */
+return <div className="app bg-deep-blue">
+  <NavBar
+    selectedPage={selectedPage}
+    setSelectedPage={setSelectedPage}
+  />
+</div>;
 }
 
 export default App;
