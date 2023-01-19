4f95e0e2d1824f80b18da4534b77720bd39db3db
mffonua <108596346+mffonua@users.noreply.github.com>
2023-01-18 19:53:13 -0700

    finshed landing page and change picture

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -2,35 +2,73 @@ import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
-const Landing = ({setSelectedPage}) => {
-    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+const Landing = ({ setSelectedPage }) => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
 
-    return(
-        <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
-                {/* Image Section */}
-                <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32"> 
-                {isAboveMediumScreens ? (
-                   
-                     <div 
-                     /**builds the border line behind profile img */
-                    className="relative z-0 ml-20 before:absolute before:-t-20 before:-left-20 
-                    before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full
-                    before:border-2 before:border-blue before:z-[-1]"
-                    >
-                        <img 
-                        alt="profile picture"
-                        className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full 
-                        max-w-[400px] md:max-w-[600px]"
-                        src="assets/profile-image.png"
-                        />
-                     </div>
-                ) : ( 
-                    <div></div>
-                )}
-                </div>
+  return (
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
+    >
+      {/* IMAGE SECTION */}
+      <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
+        {isAboveLarge ? (
+          <div
+            /**builds the border line behind profile img */
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
+            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+          >
+            <img
+              alt="profile"
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+              src="assets/profile-image.png"
+            />
+          </div>
+        ) : (
+          <img
+            alt="profile"
+            className="z-10 w-full max-w-[400px] md:max-w-[600px]"
+            src="assets/profile-image.png"
+          />
+        )}
+      </div>
 
-        </section>
-    );
+      {/* MAIN TEXT */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32">
+        {/* HEADINGS */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-6xl font-playfair z-10 text-center md:text-start">
+            Miles {""} {/**creates space before the element span  */}
+            <span
+              className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
+       before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]"
+            >
+              Fonua
+            </span>
+          </p>
+
+          <p className="mt-10 mb-7 text-sm text-center md:text-start">
+            Hello, I am currently based out of Salt Lake City, Utah working in
+            the Occupation Med. Industry. I am currently graduated from the U of
+            U Full Stack Coding Bootcamp. This bootcamp helped me build and
+            refine my JavaScrip, HTML5 and CSS skills that I have learned
+            already through my own studies. Thanks for stopping by and visiting
+            my current Portolio! If you would like to reach out to me, please
+            visit the Contact Section.
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Landing;
