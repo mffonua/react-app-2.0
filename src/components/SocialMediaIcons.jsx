3613143e1f0fb0b7e9aa127acd9d089733614fd8
mffonua <108596346+mffonua@users.noreply.github.com>
2023-01-18 21:16:15 -0700

    added in components for logo . point of contact

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,40 @@
+const SocialMediaIcons = () => {
+    return (
+      <div className="flex justify-center md:justify-start my-10 gap-7">
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.linkedin.com/in/miles-fonua-24b791237/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="linkedin-link" src="../assets/linkedin.png" />
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://github.com/mffonua"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="github-link" src="../assets/github.png" />
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.facebook.com/miles.fonua"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="facebook-link" src="../assets/facebook.png" />
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.instagram.com/miles.fonua/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="instagram-link" src="../assets/instagram.png" />
+        </a>
+      </div>
+    );
+  };
+  
+  export default SocialMediaIcons;
